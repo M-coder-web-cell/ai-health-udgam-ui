449d907e0c0c8775d1797e327c236c6967648406
tri-dip <kalitaminu464@gmail.com>
2026-01-05 17:18:10 +0530

    env added

--- src/components/MKTAI.tsx
+++ src/components/MKTAI.tsx
@@ -414,7 +414,7 @@ const MKTAI: React.FC = () => {
       }
 
       const res = await fetch(
-        `${import.meta.env.VITE_UDGAM_BACKEND_URL}/process`,
+        `${import.meta.env.VITE_BACKEND_URL}/process`,
         {
           method: 'POST',
           body: formData
